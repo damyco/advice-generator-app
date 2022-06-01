0d258cc9d2618faf27514dc126c4e37282c1df4c
damyco <25842807+damyco@users.noreply.github.com>
2022-06-01 21:38:26 +0100

    first commit

---
+++ components/Content.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import styles from "../styles/Content.module.css";
+
+export default function Content() {
+  const [advice, setAdvice] = useState({
+    data: {},
+    loading: true,
+  });
+
+  useEffect(() => {
+    fetchNewAdvice();
+  }, []);
+
+  const url = "https://api.adviceslip.com/advice";
+
+  const fetchNewAdvice = async () => {
+    try {
+      setAdvice({ data: {}, loading: true });
+      const response = await fetch(url);
+      const json = await response.json();
+      setAdvice({ data: json, loading: false });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {advice.loading ? (
+        <>
+          <p className={styles.adviceNumber}>Loading...</p>
+          <h1>
+            Preparing a new advice for you
+            <span role="img" aria-label="emoji smiley face with glasses">
+              🤓
+            </span>
+          </h1>
+        </>
+      ) : (
+        <>
+          <p className={styles.adviceNumber}>Advice #{advice.data.slip.id}</p>
+          <h1>&ldquo;{advice.data.slip.advice}&rdquo;</h1>{" "}
+        </>
+      )}
+
+      <img
+        className={styles.dividerMobile}
+        src="pattern-divider-mobile.svg"
+        alt="divider pattern"
+      />
+
+      <img
+        className={styles.dividerDesktop}
+        src="pattern-divider-desktop.svg"
+        alt="divider pattern"
+      />
+
+      <button className={styles.diceButton} onClick={fetchNewAdvice}>
+        <img src="icon-dice.svg" alt="dice icon" />
+      </button>
+    </div>
+  );
+}
